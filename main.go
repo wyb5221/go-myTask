960139d6e5a71c3659a21465a491e44aacbc843d
汪勇彬 <1453624811@qq.com>
2025-12-02 00:25:24 +0800

    gorm提交

--- main.go
+++ main.go
@@ -2,65 +2,24 @@ package main
 
 import (
 	"fmt"
-	_ "fmt"
-	_ "go-myTask/task1"
-	_ "go-myTask/task2"
-	_ "go-myTask/task3"
-	task4 "go-myTask/task4"
+	example "go-myTask/gorm1"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	// _ "go-myTask/task1"
+	// _ "go-myTask/task2"
+	// _ "go-myTask/task3"
+	// task4 "go-myTask/task4"
 )
 
 func main() {
-
-	// i := 5
-	// f := task1.IsValid("([])")
-	// fmt.Println("调用IsValid后的值i：", f)
-	// task2.Test2(&i)
-	// fmt.Println("调用test2后的值i：", i)
-	// its := []int{1, 2, 3}
-	// task2.Test4(&its)
-	// fmt.Println("调用test4后的值：", its)
-	// task2.Test5()
-	// time.Sleep(3 * time.Second)
-	// fmt.Println("main 退出")
-
-	// r := &(task3.Rectangle{10, 5})
-	// r.Area()
-	// r.Perimeter()
-	// c := &(task3.Circle{10})
-	// c.Area()
-	// c.Perimeter()
-
-	// p := task3.Person{"Golang", 20}
-	// e := &task3.Employee{1, p}
-	// e.PrintInfo()
-
-	// c := make(chan int)
-	// task3.Ch1(c)
-	// c := make(chan int, 200)
-	// c := make(chan int, 2)
-	// var w sync.WaitGroup
-	// task3.Ch2(c, w)
-	// fmt.Println("main end")
-	// c := make(chan int)
-	// task3.Ch3(c)
-	// time.Sleep(20 * time.Second)
-	// fmt.Println("main end")
-	// c := make(chan int, 100)
-	// var wg sync.WaitGroup
-	// task3.Ch4(c, wg)
-	// fmt.Println("main end")
-
-	// var wg sync.WaitGroup
-	// task4.Test1(&wg)
-	// fmt.Println("main end")
-	// c := &task4.Counter{0}
-	// task4.Test2(c)
-	// fmt.Println("--调用Test2后的c:", c)
-	// fmt.Println("--调用Test2后的值:", c.GetCount())
-	// fmt.Println("main end")
-
-	//获取结构体指针
-	c := &task4.CounterSync{}
-	task4.Test5(c)
-	fmt.Println("main end")
+	fmt.Println("----")
+	//创建数据库连接
+	db, err :=
+		gorm.Open(mysql.Open("root:123456@tcp(127.0.0.1:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"))
+	if err != nil {
+		panic(err)
+	}
+	example.Run(db)
+	fmt.Println("----")
 }
